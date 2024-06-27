85ac2f8aaebde4a8948a98b46cb0d3f1e2aff4cb
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-06-27 19:22:44 +0600

    Add files via upload

---
+++ Exceptioncodes (Airline project)/airline/BuyTicckets.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package airline;
+
+/**
+ *
+ * @author HP
+ */
+import airlineexception.IDNumberInvalidException;
+public class BuyTicckets extends Ticket{
+    void checkID(String ID,String type)
+    {
+        if("passport".equals(type) && ID.length()!=9)
+        {
+            throw new IDNumberInvalidException("Passport number is invalid");
+        }
+        
+        else if("NID".equals(type) && ID.length()!=10)
+        {
+            throw new IDNumberInvalidException("NID is invalid");
+        }
+        System.out.println("ID is valid");
+    }
+    
+}
